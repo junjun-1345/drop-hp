f3815483c58426ba284c203ebb4c808fe6aa1117
junjun-1345 <junjun.sekaowa.1345@ezweb.ne.jp>
2023-01-03 18:26:59 +0900

    ✨ 予約画面作成開始

--- src/pages/reserve.js
+++ src/pages/reserve.js
@@ -2,7 +2,7 @@ const reserve = () => {
   return (
     <div className="bg-black font-serif text-white">
       <div className="h-screen justify-center flex items-center">
-        <div>coming soon .......</div>
+        <div>ログイン・新規登録</div>
       </div>
     </div>
   );
