f1c9b01ec4698c38989b44583f3c673a574a71ab
junjun-1345 <junjun.sekaowa.1345@ezweb.ne.jp>
2023-01-27 18:22:38 +0900

    🐛 修正

--- src/pages/reserve.js
+++ src/pages/reserve.js
@@ -55,7 +55,6 @@ const reserve = () => {
               </p>
               <Link href="/checkout">
                 <button
-                  onClick={hundleSubmitTwin}
                   className="flex items-center mt-auto text-white bg-gray-400
                   border-0 py-2 px-4 w-full focus:outline-none
                   hover:bg-indigo-600 rounded"
@@ -122,10 +121,7 @@ const reserve = () => {
                 3人分のセットプランです。
               </p>
               <Link href="/checkout">
-                <button
-                  onClick={hundleSubmitTriple}
-                  className="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-indigo-600 rounded"
-                >
+                <button className="flex items-center mt-auto text-white bg-gray-400 border-0 py-2 px-4 w-full focus:outline-none hover:bg-indigo-600 rounded">
                   予約
                   <svg
                     fill="none"
