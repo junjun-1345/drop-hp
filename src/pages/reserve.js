15fb0010036be866812e86275e4b9de89941e79b
junjun-1345 <junjun.sekaowa.1345@ezweb.ne.jp>
2023-01-27 19:10:20 +0900

    🐛 最終

--- src/pages/reserve.js
+++ src/pages/reserve.js
@@ -20,7 +20,7 @@ const reserve = () => {
                 ツイン
               </h2>
               <h1 className="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">
-                ¥31,000
+                ¥31,100
               </h1>
               <p className="flex items-center text-gray-600 mb-2 mt-6">
                 <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
@@ -76,7 +76,7 @@ const reserve = () => {
                 </button>
               </Link>
               <p className="text-xs text-gray-500 mt-3">
-                支払金額には手数料1000円が含まれております。
+                ※支払金額には手数料1100円が含まれております。
               </p>
             </div>
           </div>
