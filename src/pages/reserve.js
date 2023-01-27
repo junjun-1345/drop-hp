99ce39ff9795d282178761ac03eb541b90903dac
junjun-1345 <junjun.sekaowa.1345@ezweb.ne.jp>
2023-01-27 18:54:34 +0900

    ✨ 予約

--- src/pages/reserve.js
+++ src/pages/reserve.js
@@ -19,7 +19,7 @@ const reserve = () => {
                 ツイン
               </h2>
               <h1 className="text-5xl text-gray-900 pb-4 mb-4 border-b border-gray-200 leading-none">
-                ¥13,000
+                ¥30,000
               </h1>
               <p className="flex items-center text-gray-600 mb-2 mt-6">
                 <span className="w-4 h-4 mr-2 inline-flex items-center justify-center bg-gray-400 text-white rounded-full flex-shrink-0">
@@ -53,7 +53,7 @@ const reserve = () => {
                 </span>
                 2人分のセットプランです。
               </p>
-              <Link href="/checkout">
+              <Link href="https://forms.gle/YHFXDVvVUXULwe949">
                 <button
                   className="flex items-center mt-auto text-white bg-gray-400
                   border-0 py-2 px-4 w-full focus:outline-none
@@ -80,7 +80,7 @@ const reserve = () => {
             </div>
           </div>
 
-          <div className="p-4 xl:w-1/4 md:w-1/2 w-full">
+          {/* <div className="p-4 xl:w-1/4 md:w-1/2 w-full">
             <div className="h-full p-6 rounded-lg border-2 border-gray-300 flex flex-col relative overflow-hidden">
               <h2 className="text-2xl tracking-widest title-font mb-1 font-medium">
                 トリプル
@@ -139,8 +139,8 @@ const reserve = () => {
               <p className="text-xs text-gray-500 mt-3">
                 予約確定後の人数変更は承っておりません。
               </p>
-            </div>
-          </div>
+            </div> 
+          </div>*/}
         </div>
       </div>
     </section>
